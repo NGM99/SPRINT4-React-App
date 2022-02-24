98174bed18c861283de3354f0809d17acdc1a4ba
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-02-24 10:56:53 -0600

    Iniciando sprint 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Sidebar } from "./pages/Sidebar/Sidebar";
+import { Home } from "./pages/home/Home";
+import { ProductList } from "./pages/products/ProductList";
+import { ProductView } from "./pages/productView/ProductView";
+import { Error404 } from "./components/Error404";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<ProductList />} />
+        <Route path='/products/:id' element={<ProductView />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
